02dbd7ff8767f603ef878779bcf17f27623f9fd3
marshitsky <marshitsky@gmail.com>
2022-11-14 10:25:30 +0300

    fix(project-2): fix the types

--- src/components/CountryItem/CountryItem.tsx
+++ src/components/CountryItem/CountryItem.tsx
@@ -4,19 +4,19 @@ import { Color } from "../../types";
 import { ICountry } from "../../types";
 
 interface IProps {
-  country: ICountry;
+  country: ICountry[];
 }
 
 export const CountryItem = ({ country }: IProps) => {
   return (
     <ul className="list-group">
       {Object.values(country).map(
-        ({ flags, name, capital, region, area, population }) => (
+        ({ flag, name, capital, region, area, population }) => (
           <li className="list-group-item" key={name}>
             <div className="d-flex p-2 d-flex justify-content-between">
               <div className="row row-cols-2 row-cols-lg-6 g-2 g-lg-3">
                 <div className="col d-flex align-items-center">
-                  <img src={flags} className="img-thumbnail" alt="" />
+                  <img src={flag} className="img-thumbnail" alt="" />
                 </div>
                 <div className="col d-flex align-items-center">
                   <p>
--- src/mapper/mapper.ts
+++ src/mapper/mapper.ts
@@ -2,10 +2,12 @@ import { ICountryAPI } from "../types";
 import { ICountry } from "../types";
 import countries from "../country-data.json";
 
-export const gettransformedCountries = (countries: ICountryAPI[]): any => {
+export const gettransformedCountries = (
+  countries: ICountryAPI[]
+): ICountry[] => {
   return countries.map(({ flags, name, capital, region, area, population }) => {
     return {
-      flags: flags.svg,
+      flag: flags.svg,
       name: name.common,
       capital: capital[0],
       region: region,
