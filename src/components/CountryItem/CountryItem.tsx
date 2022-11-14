6302e8776e8f62971bed2ea017fad6d697da5586
marshitsky <marshitsky@gmail.com>
2022-11-14 13:44:36 +0300

    fix(project-2): remove Object.values

--- src/components/CountryItem/CountryItem.tsx
+++ src/components/CountryItem/CountryItem.tsx
@@ -10,44 +10,42 @@ interface IProps {
 export const CountryItem = ({ country }: IProps) => {
   return (
     <>
-      {Object.values(country).map(
-        ({ flag, name, capital, region, area, population }) => (
-          <li className="list-group-item" key={name}>
-            <div className="d-flex p-2 d-flex justify-content-between">
-              <div className="row row-cols-2 row-cols-lg-6 g-2 g-lg-3">
-                <div className="col d-flex align-items-center">
-                  <img src={flag} className="img-thumbnail" alt="" />
-                </div>
-                <div className="col d-flex align-items-center">
-                  <p>
-                    <strong>{name}</strong>
-                  </p>
-                </div>
-                <div className="col d-flex align-items-center">
-                  <p>
-                    <strong>{capital}</strong>
-                  </p>
-                </div>
-                <div className="col d-flex align-items-center">
-                  <p>
-                    <strong>{region}</strong>
-                  </p>
-                </div>
-                <div className="col d-flex align-items-center">
-                  <Badge color={Color.Primary} badgeLabel="area: ">
-                    {area}
-                  </Badge>
-                </div>
-                <div className="col d-flex align-items-center">
-                  <Badge color={Color.Secondary} badgeLabel="population: ">
-                    {population}
-                  </Badge>
-                </div>
+      {country.map(({ flag, name, capital, region, area, population }) => (
+        <li className="list-group-item" key={name}>
+          <div className="d-flex p-2 d-flex justify-content-between">
+            <div className="row row-cols-2 row-cols-lg-6 g-2 g-lg-3">
+              <div className="col d-flex align-items-center">
+                <img src={flag} className="img-thumbnail" alt="" />
+              </div>
+              <div className="col d-flex align-items-center">
+                <p>
+                  <strong>{name}</strong>
+                </p>
+              </div>
+              <div className="col d-flex align-items-center">
+                <p>
+                  <strong>{capital}</strong>
+                </p>
+              </div>
+              <div className="col d-flex align-items-center">
+                <p>
+                  <strong>{region}</strong>
+                </p>
+              </div>
+              <div className="col d-flex align-items-center">
+                <Badge color={Color.Primary} badgeLabel="area: ">
+                  {area}
+                </Badge>
+              </div>
+              <div className="col d-flex align-items-center">
+                <Badge color={Color.Secondary} badgeLabel="population: ">
+                  {population}
+                </Badge>
               </div>
             </div>
-          </li>
-        )
-      )}
+          </div>
+        </li>
+      ))}
     </>
   );
 };
